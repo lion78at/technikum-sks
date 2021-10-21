c5fcad4fbf1e78fb251aa9796f14ee06eb815667
lion78at <b.loewenstein@gmx.at>
2021-10-21 17:26:40 +0200

    WARDemo finished

---
+++ Sourcen/02_WARDemo/src/main/java/at/technikumwien/HelloWorldController.java
@@ -0,0 +1,12 @@
+package at.technikumwien;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping
+    public String sayHelloAsText() {
+        return "Hello World!";
+    }
+}
